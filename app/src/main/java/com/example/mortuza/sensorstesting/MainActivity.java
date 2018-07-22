e2a13aed68dcb3c6e16bc45d27fbba7ada41b347
Mortuza <mortuzahossain1997@gmail.com>
2018-07-23 00:54:39 +0600

    Get all the sensor list and show in list view.

--- app/src/main/java/com/example/mortuza/sensorstesting/MainActivity.java
+++ app/src/main/java/com/example/mortuza/sensorstesting/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.mortuza.sensorstesting;
 
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+import android.content.Context;
+import android.hardware.Sensor;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SensorManager smm;
+    List<Sensor> sensor;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        smm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lv = findViewById(R.id.sensorList);
+        sensor = smm.getSensorList(Sensor.TYPE_ALL);
+        lv.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sensor));
+
     }
 }
